e0c67009347cb96018b04727e9451415f701277e
Kodato <kodadac85546@outlook.com>
2023-11-28 16:48:19 +0900

    add license test.bash

--- test.bash
+++ test.bash
@@ -1,4 +1,6 @@
 #!/bin/bash -xv
+# SPDX-FileCopyrightText: 2023 Ryuki Kodato
+# SPDX-Licence-Identifire: BSD-3-Clause
 
 ng () {
 	echo NG at Line $1
@@ -9,7 +11,7 @@ res=0
 
 ### I/O TEST ###
 out=$(seq 5 | ./plus)
-[ "$[out]" = 15 ] || ng $[LINENO]
+[ "${out}" = 15 ] || ng $[LINENO]
 
 ### STRANCE INPUT ###
 out=$(echo あ | ./plus)
